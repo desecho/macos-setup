c6ab65945eaafe04c80a08e81f845934d9a35047
Anton Samarchyan <desecho@gmail.com>
2022-08-07 03:47:29 -0400

    Changes

--- scripts/install_python_pkgs.sh
+++ scripts/install_python_pkgs.sh
@@ -4,4 +4,4 @@ set -eou pipefail
 
 python3 -m pip install --upgrade pip setuptools wheel
 
-pip3 install tox docker-compose bpytop thefuck poetry
+pip3 install tox docker-compose bpytop thefuck poetry tox-poetry
