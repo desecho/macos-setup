bcb2a14110e7984aefe6ed9253689f16497eafdb
Anton Samarchyan <desecho@gmail.com>
2023-06-10 10:14:04 -0400

    changes

--- scripts/install_brew_pkgs.sh
+++ scripts/install_brew_pkgs.sh
@@ -20,6 +20,7 @@ brew install noahgorstein/tap/jqp
 brew install s3cmd
 brew install shfmt
 brew install actionlint
+brew install djhtml
 brew install python@3.10
 # shellcheck disable=SC2016
 echo 'export PATH="/opt/homebrew/opt/python@3.10/bin:$PATH"' >> ~/.zshrc
